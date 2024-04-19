9cbc7554eaa97cadb67390b3a2ffa442adefe3d7
Thanh Thai <thanhthai@fulygon.com>
2024-04-19 13:27:11 +0700

    update script

--- start.sh
+++ start.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 
-appimagetool-url=https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage
+appimagetoolUrl='https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage'
 baseAppImage=$(ls | grep -i '\.AppImage$' | head -n 1)
+
+echo
+
+# Check if AppImage exists
+if [ -z $baseAppImage ]; then
+    echo "No AppImage found. Exiting..."
+    exit 1
+fi
 baseAppImagePath=$(readlink -f $baseAppImage)
 
+# User prompt
+echo
+echo "Detected AppImage: $baseAppImage"
+read -p "Start patching with this AppImage? [Y/n] " startScript
+if [ $startScript == "n" ] || [ $startScript == "N" ]; then
+    echo "Exiting..."
+    exit 0
+fi
+
 # Build navicat patcher
+echo
 echo "Building Navicat Patcher..."
 make all
 
 # Extract AppImage
+echo
 echo "Extracting AppImage..."
 mkdir -p /navicat_mount /navicat
 mount -o loop $baseAppImagePath /navicat_mount
@@ -17,21 +36,29 @@ umount /navicat_mount
 echo "Extracted AppImage."
 
 # Start Patcher
+echo
 echo "Starting Patcher..."
 /patcher/bin/navicat-patcher /navicat
 echo "Navicat Patched."
 
 # Repack
+echo
 echo "Repacking AppImage..."
-wget -O appimagetool.AppImage 'https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage'
+wget -O appimagetool.AppImage $appimagetoolUrl
 chmod +x appimagetool.AppImage
 ./appimagetool.AppImage /navicat /patcher/bin/$(echo $baseAppImage | sed 's/\.AppImage$/-patched.AppImage/')
 echo "Repacked AppImage."
 
 # Start Keygen
+echo
 echo "Starting Keygen... (REMEMBER TO DISCONNECT INTERNET)"
-read -p "Press ENTER to continue"
+read -p "Continue? [Y/n] " startKeygen
+if [ $startKeygen == "n" ] || [ $startKeygen == "N" ]; then
+    echo "Exiting..."
+    exit 0
+fi
 /patcher/bin/navicat-keygen --text ./RegPrivateKey.pem
-echo "Copy activation code and continue activa
+echo
+echo "Copy activation code and continue activation"
 echo "Exiting..."
 exit 0
